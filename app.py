220228b9716ad6a56ddda5b07ac03301c8730058
jodeon <deon_jsn@hotmail.com>
2025-04-22 17:49:00 -0300

    autentic db

--- app.py
+++ app.py
@@ -1,13 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from models.user import User
 from database import db
+from flask_login import LoginManager, login_user, current_user
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "minha_chave_secreta"  # config chave de acesso ao bando
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # caminho do arquivo, o endereço do banco
- 
+
+login_manager = LoginManager() 
 db.init_app(app)
+login_manager.init_app(app)
+
+# view login
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+     return User.query.get(user_id4)
+
+
+@app.route('/login', methods=["POST"])
+def login():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        #longin
+        user = User.query.filter_by(username=username).first()
+
+        if user and user.password == password:
+                login_user(user)
+                print(current_user.is_authenticated)
+                return jsonify({"message": " Autenticação realizada com sucesso"})
+
+    return jsonify({"message": "Credenciais inválidas"}), 400
 
 
 @app.route("/ola-mundo", methods=["GET"])
